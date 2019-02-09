1fb1c35eb2a033ccdbc16952aa6255f8130880ff
raulpesilva <adraulpesilva@gmail.com>
2019-02-09 20:19:55 -0200

    arrumando o bug finalmente

--- js/menu.js
+++ js/menu.js
@@ -9,69 +9,10 @@ $botao_fechar_menu.addEventListener('click', event => {
     
 })
 
-// window.addEventListener('resize',event =>{
-//     const $elemento = getComputedStyle($botao_fechar_menu);
-//     // console.log($elemento);
-//     if(window.innerWidth >= 799 && $elemento.display == 'flex'){
-//         $menu_nav.style.display = 'flex';
-//     }
-// })
-
-
-// $conteudo.addEventListener('click', item => {
-//         // let prevScrollpos = window.pageYOffset;
-//     console.log('a')
-//     console.log($conteudo.);
-
-//     // menuScroll();
-    
-// })
-
-
-// const prevScrollpos = window.pageYOffset;
-
-
-// window.onscroll = function () {
-
-//     const currentScrollpos = window.pageYOffset;
-
-    
-//     if( (prevScrollpos + 10) > currentScrollpos ){
-
-//         document.getElementById('menu-mobile').style.top = '0';
-        
-
-//     }else{
-//         document.getElementById('menu-mobile').style.top = '-50px';
-
-//     }
-// }
-// console.log(window.pageYOffset);
-
-// function menuScroll(prevScrollpos){
-//     let currentScrollpos = window.pageYOffset;
-
-//     if(prevScrollpos > currentScrollpos){
-//         document.getElementById('menu-mobile').style.top = '0px';
-
-//     }else{
-//         document.getElementById('menu-mobile').style.top = '-50px';
-        
-
-//     }
-// }
-
-
-
-
-
 $menu_mobile.addEventListener('click',event => {
 
     $menu_nav.style.display = 'flex';
 })
-
-
-
 
 $conteudo.addEventListener('click', event => {
     if ($menu_nav.style.display == 'flex'){
