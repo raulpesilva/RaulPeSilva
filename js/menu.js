8844882628f8ab304bc97133c49ff718737d58c2
raulpesilva <adraulpesilva@gmail.com>
2019-02-09 16:33:03 -0200

    ajuste menu mobile and resize screen + desc home

--- js/menu.js
+++ js/menu.js
@@ -1,22 +1,29 @@
 const $menu_mobile = document.querySelector('.botao-menu-mobile');
 const $menu_nav = document.querySelector('.menu-nav');
+const $conteudo = document.querySelector('.conteudo');
 const $botao_fechar_menu = document.querySelector('.botao-fechar-menu');
 
 $botao_fechar_menu.addEventListener('click', event => {
-    if($menu_nav.style.display == 'flex'){
-        // console.log($menu_nav.style.display)
-        $menu_nav.style.display = 'none';
-        
-    }
+    $menu_nav.style.display = 'none';
+    
 })
 
 window.addEventListener('resize',event =>{
-    if(window.innerWidth >= 800){
+    const $elemento = getComputedStyle($botao_fechar_menu);
+    // console.log($elemento);
+    if(window.innerWidth >= 799 && $elemento.display == 'flex'){
         $menu_nav.style.display = 'flex';
     }
 })
 
 $menu_mobile.addEventListener('click',event => {
+
     $menu_nav.style.display = 'flex';
 })
 
+
+$conteudo.addEventListener('click', event => {
+    if ($menu_nav.style.display == 'flex'){
+        $menu_nav.style.display = 'none'
+    }
+})
