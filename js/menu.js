e6b1e610f9890cac2c55ca3fc64a7ae778be6c1b
raulpesilva <adraulpesilva@gmail.com>
2019-02-11 21:23:12 -0200

    animate menu

--- js/menu.js
+++ js/menu.js
@@ -7,28 +7,36 @@ const $botao_fechar_menu = document.querySelector('.botao-fechar-menu');
 $menu_mobile.addEventListener('click', event =>{
 
     if(window.innerWidth < 800){
-        $menu_nav.style.display = 'flex';
+
+        $menu_nav.style.left = '0';
     }
 })
 
 $botao_fechar_menu.addEventListener('click', event => {
-    $menu_nav.style.display = 'none';
-    
+    $menu_nav.style.left = '-221px';
+   
 })
 
 window.addEventListener('resize',event =>{
 
     if(window.innerWidth >= 800){
-        $menu_nav.style.display = 'flex';
+
+        $menu_nav.style.left = '0';
+
     }else{
-        $menu_nav.style.display = 'none';
+
+        $menu_nav.style.left = '-221px';
+
     }
 })
 
 $conteudo.addEventListener('click',event => {
-    
-    if(window.innerWidth < 800 && $menu_nav.style.display == 'flex'){
-        $menu_nav.style.display = 'none';
+    const navPosition = getComputedStyle($menu_nav).left
+    if(window.innerWidth < 800 && navPosition == '0px'){
+
+        $menu_nav.style.left = '-221px';
+        
+
         
     }
 })
