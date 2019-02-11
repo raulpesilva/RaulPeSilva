3e32ba3f112c601b1eb9e4577546287334c16def
raulpesilva <adraulpesilva@gmail.com>
2019-02-11 20:34:39 -0200

    fix all bugs menu and space bottom home

--- js/menu.js
+++ js/menu.js
@@ -4,35 +4,33 @@ const $conteudo = document.querySelector('.conteudo');
 const $botao_fechar_menu = document.querySelector('.botao-fechar-menu');
 
 
+$menu_mobile.addEventListener('click', event =>{
+
+    if(window.innerWidth < 800){
+        $menu_nav.style.display = 'flex';
+    }
+})
+
 $botao_fechar_menu.addEventListener('click', event => {
     $menu_nav.style.display = 'none';
     
 })
 
 window.addEventListener('resize',event =>{
-    // console.log($elemento);
-    
-    console.log(window.innerWidth);
+
     if(window.innerWidth >= 800){
         $menu_nav.style.display = 'flex';
-        $conteudo.addEventListener('click', event => {
-            if ($menu_nav.style.display == 'none'){
-                $menu_nav.style.display = 'flex'
-            }
-        })
     }else{
         $menu_nav.style.display = 'none';
-        $conteudo.addEventListener('click', event => {
-            if ($menu_nav.style.display == 'flex'){
-                $menu_nav.style.display = 'none'
-            }
-        })
-
     }
 })
 
-$menu_mobile.addEventListener('click',event => {
-
-    $menu_nav.style.display = 'flex';
+$conteudo.addEventListener('click',event => {
+    
+    if(window.innerWidth < 800 && $menu_nav.style.display == 'flex'){
+        $menu_nav.style.display = 'none';
+        
+    }
 })
+
 
